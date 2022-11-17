074d396da17262da63b01c85b47a46d9966a878c
kingsleycj <primekings.kc@gmail.com>
2022-11-18 00:27:36 +0100

    webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,8 @@
 // import '../styles/index.scss';
 
 // console.log('webpack starterkit');
+function sendCars ( ...allCarIds) {
+    allCarIds.forEach(id => console.log(id));
+};
 
-console.log('Hello world!');
+sendCars('monday', 1, 2, 5);
