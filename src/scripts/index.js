7459768c6f1a047cac56063beacf293764a57a2f
kingsleycj <primekings.kc@gmail.com>
2022-11-19 21:13:53 +0100

    Rest parameters

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,9 @@
 // import '../styles/index.scss';
 
 // console.log('webpack starterkit');
-function sendCars ( ...allCarIds) {
+function sendCars (c...allCarIds) {
     allCarIds.forEach(id => console.log(id));
-};
+}
 
 sendCars('monday', 1, 2, 5);
+
