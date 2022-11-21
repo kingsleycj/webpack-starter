649597350abd705fdc2c8b51eb348710749b0863
kingsleycj <primekings.kc@gmail.com>
2022-11-21 11:20:39 +0100

    Spread Syntax

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,31 @@
 // import '../styles/index.scss';
 
 // console.log('webpack starterkit');
-function sendCars (...allCarIds) {
-    allCarIds.forEach(id => console.log(id));
+//REST parameters
+// function sendCars (...allCarIds) {
+//     allCarIds.forEach(id => console.log(id));
+// }
+
+// sendCars('monday', 1, 2, 5);
+
+// Destructing Arrays
+// let carIds = [1, 2, 5]
+// let car1, car2, theRest;
+// [car1, car2, ...theRest] = carIds;
+// console.log(car1, car2, theRest);
+
+// Destructing Objects
+let car = {id: 5000, style: 'convertible'};
+let id, style;
+({id, style} = car);
+// console.log(id, style);
+
+// Spread Syntax
+function startCars(car1, car2, ...rest){
+    console.log(rest);
 }
 
-sendCars('monday', 1, 2, 5);
+let carIds = [1, 2, 3, 4, 5, 6];
+startCars(...carIds);
+
 
